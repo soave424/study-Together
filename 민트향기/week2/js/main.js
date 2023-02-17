8fb65ae64bd8272604f69fa679eced2c8cda49a5
민트향기 <69835453+mintkaori@users.noreply.github.com>
2023-02-17 11:44:57 +0900

    #2.6 Eraser (05:27)

--- 민트향기/week2/js/main.js
+++ 민트향기/week2/js/main.js
@@ -1,13 +1,22 @@
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const earaserBtn = document.getElementById("eraser-btn");
 const colorOptions = Array.from(document.getElementsByClassName('color-option'));
 const color = document.getElementById('color');
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
 ctx.linewidth = lineWidth.value;
 
 let isPainting = false;
+let isFilling = false;
 
 //flatuicolors.com
 const colors = [
@@ -49,16 +58,49 @@ function onColorClick(e){
 	color.value =  colorValue;
 
 }
+
+function onModeClick(){
+	if (isFilling) {
+		isFilling = false
+		modeBtn.innerText = "Fill"
+	} else {
+		isFilling = true
+		modeBtn.innerText = "Draw"
+	}
+}
 function onChangeColor(e) {
 	ctx.strokeStyle = e.target.value;
 	ctx.fillStyle = e.target.value;
 }
+
+function onCanvasClick() {
+	if(isFilling) {
+		ctx.fillRect(0, 0, 800, 800);
+	}
+}
+
+function onDestroyClick() {
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, 800, 800);
+}
+
+function onEraserClick(){
+ 	ctx.strokeStyle = "white";
+ 	isFilling = false;
+ 	modeBtn.innerText = "Fill"
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onChangeColor);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+earaserBtn.addEventListener("click", onEraserClick);
