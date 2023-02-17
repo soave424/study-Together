20c67a41104fc192f4f3072f25fee9d77db6d51b
민트향기 <69835453+mintkaori@users.noreply.github.com>
2023-02-17 11:02:35 +0900

    #2.4 Paint Color part Two (09:34)

--- 민트향기/week2/js/main.js
+++ 민트향기/week2/js/main.js
@@ -1,4 +1,6 @@
-const lineWidth = document.getElementById("line-width")
+const colorOptions = Array.from(document.getElementsByClassName('color-option'));
+const color = document.getElementById('color');
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
@@ -40,9 +42,23 @@ function onLineWidthChange(e) {
 	ctx.lineWidth = e.target.value;
 }
 
+function onColorClick(e){
+	const colorValue = e.target.dataset.color
+	ctx.strokeStyle = colorValue;
+	ctx.fillStyle = colorValue;
+	color.value =  colorValue;
+
+}
+function onChangeColor(e) {
+	ctx.strokeStyle = e.target.value;
+	ctx.fillStyle = e.target.value;
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onChangeColor);
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
