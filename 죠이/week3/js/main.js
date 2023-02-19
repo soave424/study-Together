93120a75d6b4f31f4ba49c00c5e4287e3e5a4d0f
soave424 <soave424@gmail.com>
2023-02-19 16:03:19 +0900

    file upload

--- 죠이/week3/js/main.js
+++ 죠이/week3/js/main.js
@@ -1,24 +1,15 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 800;
-
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
-
-//굵기 지정 불러오기
 const lineWidth = document.getElementById("line-width");
-ctx.lineWidth = lineWidth.ariaValueMax;
-
-//색 지정 불러오기
 const colorPick = document.getElementById("color-pick");
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const fileInput = document.getElementById("file");
 
-//색 옵션 불러오기
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
-
 const colors = [
   "#ff3838",
   "#ffb8b8",
@@ -31,6 +22,17 @@ const colors = [
   "#7d5fff",
 ];
 
+let isPainting = false;
+let isFilling = false;
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+ctx.lineWidth = lineWidth.ariaValueMax;
+
 function onClick(event) {
   //같은 path에 있으면 선 색깔이 같음.
   ctx.beginPath();
@@ -40,9 +42,6 @@ function onClick(event) {
   ctx.lineTo(event.offsetX, event.offsetY);
   ctx.stroke();
 }
-
-//2.1 mouse 움직이면서 만들기
-let isPainting = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -60,40 +59,22 @@ function cancelPainting() {
   isPainting = false;
   ctx.beginPath();
 }
-canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", onMouseDown);
-canvas.addEventListener("mouseup", cancelPainting);
 
-//canvas 밖으로 나간 경우 mouseup과 같은 기능 실행되도록
-canvas.addEventListener("mouseleave", cancelPainting);
-
-//2.2 선의 굵기 변경하기
 function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
 
-lineWidth.addEventListener("change", onLineWidthChange);
-
-//2.3 색 변경하기
 function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
 }
 
-//2.4 컬러 옵션으로 색 변경하기
 function onColorClick(event) {
   const colorValue = event.target.dataset.color;
   ctx.strokeStyle = colorValue;
   ctx.fillStyle = colorValue;
   colorPick.value = colorValue;
 }
-
-colorPick.addEventListener("change", onColorChange);
-colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
-
-//2.5 filling Mode
-const modeBtn = document.getElementById("mode-btn");
-let isFilling = false;
 
 function onModeClick() {
   if (isFilling) {
@@ -111,13 +92,6 @@ function onCanvasClick() {
   }
 }
 
-canvas.addEventListener("click", onCanvasClick);
-modeBtn.addEventListener("click", onModeClick);
-
-//2.6 Eraser
-const destroyBtn = document.getElementById("destroy-btn");
-const eraserBtn = document.getElementById("eraser-btn");
-
 function onDestroyClick() {
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -128,5 +102,26 @@ function onEraserClick() {
   isFilling = false;
   modeBtn.innerText = "Fill";
 }
+
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+lineWidth.addEventListener("change", onLineWidthChange);
+colorPick.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+canvas.addEventListener("click", onCanvasClick);
+modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
